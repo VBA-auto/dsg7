47ee569126e551cce33a7e56a47dd20584d3042e
chowon hasan <chowonhasan7@gmail.com>
2025-03-09 04:12:25 +0600

    dfsdfs

--- src/app/produits/[url]/page.jsx
+++ src/app/produits/[url]/page.jsx
@@ -1,8 +1,5 @@
 // app/shop/[id]/page.jsx
-import ResponsiveSlider from "@/app/Components/ResSlider";
-import SizeChart from "@/app/Components/SizeChart";
-import RecommendedProducts from "@/app/Components/RecommndProduct";
-import CartDrawer from "@/app/Components/CartDrawer";
+import Image from "next/image";
 
 async function fetchProduct(url) {
   try {
@@ -29,60 +26,18 @@ export default async function ProductPage({ params }) {
       <div className="max-w-7xl mx-auto my-8">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
           <div className="w-2/3 ms-auto">
-            <ResponsiveSlider images={product.gallary} />
+            <Image width={600} height={500} src={product.image} alt="sd" />
           </div>
 
           {/* Product Details */}
           <div>
-            <h1 className="text-2xl font-bold mt-2">{product.name}</h1>
+            <h1 className="text-2xl font-bold mt-2">{product.title}</h1>
             <p className="text-gray-600 mt-2">{product.description}</p>
 
             {/* Price */}
             <p className="text-2xl font-semibold text-blue-500 mt-4">
-              ৳{product.price}
+              {product.price}
             </p>
-
-            {/* Display Available Sizes */}
-            <p className="mt-3 font-semibold">Available Sizes:</p>
-            <div className="flex space-x-2 mt-1">
-              {product.sizes.map((size, index) => (
-                <span
-                  key={index}
-                  className="px-3 py-1 border rounded-md bg-gray-200 text-sm"
-                >
-                  {size}
-                </span>
-              ))}
-            </div>
-
-            {/* Display Available Colors */}
-            <p className="mt-3 font-semibold">Available Colors:</p>
-            <div className="flex space-x-2 mt-1">
-              {product.colors.map((color, index) => (
-                <span
-                  key={index}
-                  className="w-6 h-6 rounded-full border"
-                  style={{ backgroundColor: color.toLowerCase() }}
-                ></span>
-              ))}
-            </div>
-
-            {/* Material */}
-            <p className="mt-3 font-semibold">
-              Material:
-              <span className="font-normal text-gray-700">
-                {" "}
-                {product.material}
-              </span>
-            </p>
-
-            {/* Features */}
-            <p className="mt-3 font-semibold">Features:</p>
-            <ul className="list-disc list-inside text-gray-600">
-              {product.features.map((feature, index) => (
-                <li key={index}>{feature}</li>
-              ))}
-            </ul>
             <div className="">
               <label
                 htmlFor="my-drawer-4"
@@ -91,17 +46,8 @@ export default async function ProductPage({ params }) {
                 Add to carts
               </label>
             </div>
-            <div className="">
-              <SizeChart />
-            </div>
           </div>
         </div>
-
-        {/* Pass the product data to the CartDrawer component */}
-        <CartDrawer product={product} />
-
-        {/* Recommended Products */}
-        <RecommendedProducts currentProductId={url} />
       </div>
     </main>
   );

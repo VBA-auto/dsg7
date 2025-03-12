6f31bfaf4cc0ff287eff68af485efa476f905a76
Chowon-Hasan <chowonhasan7@gmail.com>
2025-03-13 02:29:16 +0600

    dsdsdfdsf

--- src/app/Components/Header/Header.jsx
+++ src/app/Components/Header/Header.jsx
@@ -52,7 +52,7 @@ export default function Header() {
                 isScrolled ? "text-red-500" : "text-red-500"
               }`}
             >
-              About Us
+              Qui sommes nous ?
             </span>
             <span className="absolute left-0 bottom-0 h-0.5 w-0 bg-red-500 transition-all duration-300 group-hover:w-full" />
           </Link>
